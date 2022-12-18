2e8012ba8adaf75d4c9b4b8b7fb1cd08e50ba912
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2022-12-17 22:07:19 -0400

    commit

--- stickynav.js
+++ stickynav.js
@@ -1,7 +1,7 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyNav()};
 var navbar = document.getElementsByClassName("navbar")[0];
 var sticky = navbar.offsetTop;
-function myFunction() {
+function stickyNav() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
